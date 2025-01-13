095cc54eef9261f412fcc7c97cc48a8dae6ed867
haruoogawa <haru.spring.yodai@gmail.com>
2025-01-14 08:37:51 +0900

    ライトの仮制御を実装

--- Src/Component/CBeamLightController.cpp
+++ Src/Component/CBeamLightController.cpp
@@ -9,8 +9,8 @@ namespace component
 		m_Base_RotYaw(nullptr),
 		m_Base_RotPitch(nullptr)
 	{
-		//GetValueRegistry()->SetValue("Rot_Yaw", graphics::EUniformValueType::VALUE_TYPE_FLOAT, &glm::vec1(0.0f)[0], sizeof(float));
-		//GetValueRegistry()->SetValue("Rot_Pitch", graphics::EUniformValueType::VALUE_TYPE_FLOAT, &glm::vec1(-90.0f)[0], sizeof(float));
+		GetValueRegistry()->SetValue("Rot_Yaw", graphics::EUniformValueType::VALUE_TYPE_FLOAT, &glm::vec1(0.0f)[0], sizeof(float));
+		GetValueRegistry()->SetValue("Rot_Pitch", graphics::EUniformValueType::VALUE_TYPE_FLOAT, &glm::vec1(-90.0f)[0], sizeof(float));
 	}
 
 	CBeamLightController::~CBeamLightController()
@@ -32,7 +32,7 @@ namespace component
 		if (!BeamLight) return true;
 
 		m_Base_RotYaw = BeamLight->FindNodeByName("Base_RotYaw");
-		m_Base_RotPitch = BeamLight->FindNodeByName("Base_RotPitch");
+		m_Base_RotPitch = BeamLight->FindNodeByName("Beam_Origin");
 
 		return true;
 	}
@@ -42,6 +42,12 @@ namespace component
 		const std::shared_ptr<graphics::CDrawInfo>& DrawInfo, const std::shared_ptr<input::CInputState>& InputState)
 	{
 		if (!m_Base_RotYaw || !m_Base_RotPitch) return true;
+
+		float Yaw = glm::sin(DrawInfo->GetSecondsTime()) * 45.0f;
+		float Pitch = (glm::sin(DrawInfo->GetSecondsTime()) * 0.5f + 0.5f) * 90.0f;
+
+		m_Base_RotYaw->SetRot(glm::angleAxis(glm::radians(Yaw), glm::vec3(0.0f, 1.0f, 0.0f)));
+		m_Base_RotPitch->SetRot(glm::angleAxis(glm::radians(Pitch), glm::vec3(1.0f, 0.0f, 0.0f)));
 
 		return true;
 	}
